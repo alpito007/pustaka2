c40344331116436e59229da07e5c48155f4eb501
alpito007 <alpitogilang07@gmail.com>
2022-11-30 12:53:37 +0700

    alpito gilang

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Ilham Kurniawan
+        Saya tingga di daerah Karawang
+        olah raga yang saya sukai adalah
+        Bulutangkis";
+    }
+    
+}
